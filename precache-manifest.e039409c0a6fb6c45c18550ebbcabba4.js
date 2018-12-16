80191c5d7b2309a4be9281a45fac64781e745a3c
Aamir Jutt <ars3753669@gmail.com>
2018-12-16 11:57:00 +0500

    Updates

---
+++ precache-manifest.e039409c0a6fb6c45c18550ebbcabba4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e258ea1747dec9816919",
+    "url": "/home/static/js/runtime~main.e258ea17.js"
+  },
+  {
+    "revision": "c0432b37eda74b4a9260",
+    "url": "/home/static/js/main.c0432b37.chunk.js"
+  },
+  {
+    "revision": "6dc6fa5e7042f224a988",
+    "url": "/home/static/js/1.6dc6fa5e.chunk.js"
+  },
+  {
+    "revision": "c0432b37eda74b4a9260",
+    "url": "/home/static/css/main.35d1ef6b.chunk.css"
+  },
+  {
+    "revision": "6dc6fa5e7042f224a988",
+    "url": "/home/static/css/1.2c543616.chunk.css"
+  },
+  {
+    "revision": "868de4e635a1e15cf0f66d47c509f9b4",
+    "url": "/home/index.html"
+  }
+];
